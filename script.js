2c3fbfa8a8f0588cc4d943a3e8dcc49122a923c3
pedrolima2106 <pedrolima2106@gmail.com>
2025-03-14 00:55:44 -0300

    primeiro comite de teste

---
+++ script.js
@@ -0,0 +1,4 @@
+// Seleciona o botão pelo ID e adiciona um evento de clique
+document.getElementById("meuBotao").addEventListener("click", function() {
+    alert("Botão clicado!"); // Exibe um alerta ao clicar no botão
+});
